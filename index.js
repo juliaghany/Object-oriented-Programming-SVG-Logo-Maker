6d6e7e9c7603a3811dd124b4b944047fd768141e
Jules <juliaghany@msn.com>
2023-03-31 13:46:51 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// include packages needed for application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateSvg = require("./utils/generateSvg");
+
+const questions = [];
