2f0f0ff56799b926a4825cef839c9c36a43c7449
Jules <juliaghany@msn.com>
2023-04-03 13:11:27 -0500

    modified questions to resemble 09-NodeJS activity 19 because I'm currently using that activity as a reference for this part of the challenge

--- index.js
+++ index.js
@@ -2,32 +2,34 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-const svg = require("./lib/shapes");
+const svg = require("./lib/shapes.js");
 
 // array of questions for user input
 
-const questions = [
-    {
-        type: "input",
-        name: "characters",
-        message: "Let's create a logo! Please choose up to three characters to include in your logo."
-    },
-    {
-        type: "input",
-        name: "text-color",
-        message: "Please choose a color for the text in your logo by entering the color's keyword or hexadecimal number."
-    },
-    {
-        type: "list",
-        name: "shape",
-        message: "Which shape would you like to include in your logo?",
-        choices: ["Circle", "Triangle", "Square"]
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "characters",
+            message: "Let's create a logo! Please choose up to 3 characters to include in your logo."
+        },
+        {
+            type: "input",
+            name: "text-color",
+            message: "Please choose a color for the text in your logo by entering the color's keyword or a hexadecimal number."
+        },
+        {
+            type: "list",
+            name: "shape",
+            message: "Which shape would you like to include in your logo?",
+            choices: ["Circle", "Triangle", "Square"]
 
-    },
-    {
-        type: "input",
-        name: "shape-color",
-        message: "Please choose a color for the shape in your logo by entering the color's keyword or hexadecimal number.",
-    },
-];
+        },
+        {
+            type: "input",
+            name: "shape-color",
+            message: "Please choose a color for the shape in your logo by entering the color's keyword or a hexadecimal number.",
+        },
+    ])
+    .then(())
 
