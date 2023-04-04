f731e47dab78324dd2dc8a2ff582f1898eb83b5b
Jules <juliaghany@msn.com>
2023-04-04 10:47:20 -0500

    decided to set up questions and writeToFile() and init() function differently, referenced challenge-9 to do so

--- index.js
+++ index.js
@@ -2,34 +2,49 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-const svg = require("./lib/shapes.js");
+const generateLogo = require("./lib/shapes.js");
 
 // array of questions for user input
 
-inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "characters",
-            message: "Let's create a logo! Please choose up to 3 characters to include in your logo."
-        },
-        {
-            type: "input",
-            name: "text-color",
-            message: "Please choose a color for the text in your logo by entering the color's keyword or a hexadecimal number."
-        },
-        {
-            type: "list",
-            name: "shape",
-            message: "Which shape would you like to include in your logo?",
-            choices: ["Circle", "Triangle", "Square"]
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Let's create a logo! Please choose up to 3 characters to include in your logo."
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Please choose a color for the text in your logo by entering the color's keyword or a hexadecimal number."
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "Which shape would you like to include in your logo?",
+        choices: ["Circle", "Triangle", "Square"]
 
-        },
-        {
-            type: "input",
-            name: "shape-color",
-            message: "Please choose a color for the shape in your logo by entering the color's keyword or a hexadecimal number.",
-        },
-    ])
-    .then(())
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Please choose a color for the shape in your logo by entering the color's keyword or a hexadecimal number.",
+    },
+];
+ 
+function writeToFile(fileName, data) {
+    fileName = `logo.svg`
+    fs.writeFile(fileName, generateLogo(data), (err) =>
+    err ? console.log(err) : console.log('Generated logo.svg'))
+}
 
+function init() {
+    inquirer.prompt(questions)
+        .then((answers => {
+        writeToFile("logo.svg", answers)
+    }))
+}
+
+init();
+
+
+    
