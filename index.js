6f310f063e61e50989be94c769f7b1fb876a2c91
Jules <juliaghany@msn.com>
2023-04-04 14:14:51 -0500

    add curly braces around generateLogo on line 5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateLogo = require("./lib/shapes.js");
+const { generateLogo }  = require("./lib/shapes.js");
 
 // array of questions for user input
 
